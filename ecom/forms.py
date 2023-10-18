5555af1d722b45a557f66668a7ebaef35081b635
Devyani Sarode <devyanisarode2@gmail.com>
2023-10-18 18:23:30 +0530

    first commit

---
+++ ecom/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+
+class CustomerUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+        
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model=models.Customer
+        fields=['address','mobile','profile_pic']
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerForm, self).__init__(*args, **kwargs)
+        self.fields['profile_pic'].required = True
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model=models.Product
+        fields=['name','price','description','product_image']
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.fields['product_image'].required = True
+
+#address of shipment
+class AddressForm(forms.Form):
+    Email = forms.EmailField()
+    Mobile= forms.IntegerField()
+    Address = forms.CharField(max_length=500)
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model=models.Feedback
+        fields=['name','feedback']
+
+#for updating status of order
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model=models.Orders
+        fields=['status']
+
+#for contact us page
+class ContactusForm(forms.Form):
+    Name = forms.CharField(max_length=30)
+    Email = forms.EmailField()
+    Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
